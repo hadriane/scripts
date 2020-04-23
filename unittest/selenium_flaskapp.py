5d3d6ebb4cc8404c37cedbf71696f2f5335874ca
hadriane <60728086+hadriane@users.noreply.github.com>
2020-04-23 18:02:40 +0800

    Create selenium_flaskapp.py

---
+++ unittest/selenium_flaskapp.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.PhantomJS()
+
+for x in range(6):
+	driver.get("http://3.234.207.87:5000/")
+	driver.set_window_size(550, 696)
+	driver.find_element(By.NAME, "name").click()
+	driver.find_element(By.NAME, "name").send_keys("selenium_" + str(x))
+	driver.find_element(By.CSS_SELECTOR, "form").click()
+	driver.find_element(By.NAME, "email").click()
+	driver.find_element(By.NAME, "email").send_keys("selenium_" + str(x) + "@test.com")
+	driver.find_element(By.CSS_SELECTOR, "input:nth-child(5)").click()
+driver.close()
+  
